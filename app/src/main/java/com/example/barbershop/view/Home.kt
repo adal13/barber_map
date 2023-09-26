d19c09b13b8270dada428c6a727870deaaf1af27
Jared <adal13gomez.agg@gmail.com>
2023-09-26 01:48:18 -0600

    Correccion de nombres de las opciones

--- app/src/main/java/com/example/barbershop/view/Home.kt
+++ app/src/main/java/com/example/barbershop/view/Home.kt
@@ -3,14 +3,12 @@ package com.example.barbershop.view
 import android.app.Dialog
 import android.content.ClipData.Item
 import android.content.Intent
+import android.content.res.Configuration
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.Gravity
-import android.view.View
-import android.view.ViewConfiguration
-import android.view.ViewGroup
-import android.view.Window
+import android.util.Log
+import android.view.*
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -19,6 +17,7 @@ import androidx.annotation.NonNull
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -31,9 +30,11 @@ import com.google.android.material.navigation.NavigationView
 
 
 open class Home : AppCompatActivity() {
+//    , NavigationView.OnNavigationItemSelectedListener
     private lateinit var fab: FloatingActionButton;
     private lateinit var drawerLayout: DrawerLayout;
     private lateinit var bottomNavigationView: BottomNavigationView;
+    private lateinit var toggle: ActionBarDrawerToggle;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -47,15 +48,32 @@ open class Home : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar);
 
         setSupportActionBar(toolbar);
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        if(savedInstanceState == null){
-            supportFragmentManager.beginTransaction().replace(R.id.frame_layout, HomeFragment()).commit();
-            navigationView.setCheckedItem(R.id.nav_home);
-
+//        if(savedInstanceState == null){
+//            supportFragmentManager.beginTransaction().replace(R.id.frame_layout, HomeFragment()).commit();
+//            navigationView.setCheckedItem(R.id.nav_home);
+//        }
+        navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> Toast.makeText(applicationContext, "Clicked home", Toast.LENGTH_SHORT).show()
+                R.id.nav_settings -> Toast.makeText(applicationContext, "Clicked settings", Toast.LENGTH_SHORT).show()
+                R.id.nav_share -> Toast.makeText(applicationContext, "Clicked share", Toast.LENGTH_SHORT).show()
+                R.id.nav_about -> Toast.makeText(applicationContext, "Clicked about", Toast.LENGTH_SHORT).show()
+//                {
+//                    Log.d("Navigation", "Elemento de menú 'Home' seleccionado")
+//                    val intent = Intent(applicationContext, MainActivity::class.java)
+//                    startActivity(intent)
+//                    true
+//                }
+//                else -> false
+            }
+            true
         }
+
+
         replaceFragment(HomeFragment())
 
         bottomNavigationView.setBackground(null)
@@ -122,6 +140,31 @@ open class Home : AppCompatActivity() {
         dialog.getWindow()?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         dialog.getWindow()?.getAttributes()?.windowAnimations = R.style.DialogAnimation
         dialog.getWindow()?.setGravity(Gravity.BOTTOM)
+    }
+
+//    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+//        when(item.itemId){
+//            R.id.nav_home -> Toast.makeText(this@Home, "Item 1", Toast.LENGTH_SHORT).show()
+//        }
+//        drawerLayout.closeDrawer(GravityCompat.START)
+//        return true
+//    }
+//
+//    override fun onPostCreate(savedInstanceState: Bundle?){
+//        super.onPostCreate(savedInstanceState)
+//        toggle.syncState()
+//    }
+//
+//    override fun onConfigurationChanged(newConfig: Configuration) {
+//        super.onConfigurationChanged(newConfig)
+//        toggle.onConfigurationChanged(newConfig)
+//    }
+//
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
