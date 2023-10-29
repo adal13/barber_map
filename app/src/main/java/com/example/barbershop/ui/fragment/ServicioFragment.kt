a0928d88d997b05ac631660b45598622eae51130
Jared <adal13gomez.agg@gmail.com>
2023-10-29 00:08:26 -0600

    Llamando mi ServiceAdapter, para poder devolver la informacion que se va a visualizar en mi diseño de tarjetas

--- app/src/main/java/com/example/barbershop/ui/fragment/ServicioFragment.kt
+++ app/src/main/java/com/example/barbershop/ui/fragment/ServicioFragment.kt
@@ -5,7 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.barbershop.ui.Api.client.ApiClient
+import com.example.barbershop.ui.Api.entity.Services
 import com.example.barbershop.R
+import com.example.barbershop.ui.adapter.LocalAdapter
+import com.example.barbershop.ui.adapter.ServiceAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,7 +45,33 @@ class ServicioFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_servicio, container, false)
+        val view = inflater.inflate(R.layout.fragment_servicio, container, false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Llamada a la API para obtener la lista de locales
+        val retrofitTraer = ApiClient.consumirApi.getIdServices()
+        retrofitTraer.enqueue(object : Callback<Services> {
+            override fun onResponse(call: Call<Services>, response: Response<Services>) {
+                if (response.isSuccessful) {
+                    val services = response.body()
+                    val recyclerView = view.findViewById<RecyclerView>(R.id.service_recyclerView)
+                    recyclerView.layoutManager = LinearLayoutManager(context)
+                    val adapter = services?.let { ServiceAdapter(it.`object`) } // Reemplaza "locals" con tu lista de datos
+                    recyclerView.adapter = adapter
+
+                } else {
+                    Toast.makeText(context, "Error de conexión a la API 1", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<Services>, t: Throwable) {
+                Toast.makeText(context, "Error de conexión a la API 2", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     companion object {
