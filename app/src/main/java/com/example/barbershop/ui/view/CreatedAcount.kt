089841f72ba66e4231d296723c562108d95af765
Jared <adal13gomez.agg@gmail.com>
2023-10-29 00:21:22 -0600

    Archivo creados para crear formulario de crear cuenta

---
+++ app/src/main/java/com/example/barbershop/ui/view/CreatedAcount.kt
@@ -0,0 +1,12 @@
+package com.example.barbershop.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.barbershop.R
+
+class CreatedAcount : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_created_acount)
+    }
+}
