5b1148aec1a4e6dff0970879d9ede8c185fd5561
Jared <adal13gomez.agg@gmail.com>
2023-10-29 00:10:24 -0600

    Creando archivo para poder manipular color del cursor de texto, y llamando en mi MainActivity llamando la funcion que se creo en CursorUtils para darle color a mi cursor

--- app/src/main/java/com/example/barbershop/MainActivity.kt
+++ app/src/main/java/com/example/barbershop/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.example.barbershop
 
 import android.content.Intent
+import android.graphics.PorterDuff
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.barbershop.Api.client.ApiClient
-import com.example.barbershop.Api.entity.User
+import androidx.core.content.ContextCompat
+import com.example.barbershop.ui.Api.client.ApiClient
+import com.example.barbershop.ui.Api.entity.User
 import com.example.barbershop.databinding.ActivityMainBinding
 import com.example.barbershop.ui.view.Home
+//import org.mindrot.jbcrypt.BCrypt
+import at.favre.lib.crypto.bcrypt.BCrypt
+import com.example.barbershop.ui.files.changeCursorColor
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.lang.reflect.Field
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,33 +36,8 @@ class MainActivity : AppCompatActivity() {
         val tx_password = findViewById<EditText>(R.id.tx_password)
         val btn_login = findViewById<Button>(R.id.btn_login)
 
-        /*val apiClient = ApiClient()
-        Log.d("MainActivity", "Antes de obtener apiService")
-        val apiService: ApiServices? = apiClient.getApiService()
-        Log.d("MainActivity", "Después de obtener apiService")
-
-        lifecycleScope.launch {
-            try {
-                val response = apiService?.getId()
-                if (response != null) {
-                    if (response.isSuccessful) {
-                        val userList = response.body()?.`object`
-                        if (userList != null) {
-                            for (user in userList) {
-                                println("Nombre: ${user.nombre}, Apellido: ${user.apellido}")
-                                // Realiza las operaciones necesarias con cada usuario
-                            }
-                        }
-                    } else {
-                        // Maneja errores de respuesta HTTP aquí
-                    }
-                }
-            } catch (e: Exception) {
-                // Maneja cualquier excepción que pueda ocurrir durante la llamada a la API
-                println("Error: ${e.message}")
-            }
-        }
-*/
+        changeCursorColor(tx_username, this)
+        changeCursorColor(tx_password, this)
 
         val retrofitTraer = ApiClient.consumirApi.getIdUser()
         retrofitTraer.enqueue(object : Callback<User> {
@@ -67,17 +50,20 @@ class MainActivity : AppCompatActivity() {
                         var isPasswordCorrect = false
 
                         for (users in users.orEmpty()) {
-                            //if (users.nombreUsuario == username && BCrypt.checkpw(password, users.contrasena)) {
-                            if (users.nombreUsuario == username && users.contrasena == password) {
-                                isPasswordCorrect = true
-                                val intent = Intent(this@MainActivity, Home::class.java)
-                                startActivity(intent)
-                                Toast.makeText(this@MainActivity, "Bienvenido a su cuenta ${users.nombre}", Toast.LENGTH_SHORT).show()
-                                break
+                            if (users.nombreUsuario == username) {
+                                if (BCrypt.verifyer().verify(password.toCharArray(), users.contrasena).verified) {
+                                //if (users.nombreUsuario == username && BCrypt.checkpw(password, users.contrasena)) {
+                                //if (users.nombreUsuario == username && users.contrasena == password) {
+                                    isPasswordCorrect = true
+                                    val intent = Intent(this@MainActivity, Home::class.java)
+                                    startActivity(intent)
+                                    Toast.makeText(this@MainActivity, "Bienvenido a su cuenta ${users.nombre}", Toast.LENGTH_SHORT).show()
+                                    break
+                                }
                             }
                         }
                         if (!isPasswordCorrect) {
-                            Toast.makeText(this@MainActivity,"Contraseña incorrecta",Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this@MainActivity,"Usuario o contraseña incorrecta",Toast.LENGTH_SHORT).show()
                         }
                     }
                 } else {
@@ -88,31 +74,5 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this@MainActivity, "Error al consultar", Toast.LENGTH_SHORT).show()
             }
         })
-
-/*        btn_login.setOnClickListener {
-            val username = tx_username.text.toString()
-            val password = tx_password.text.toString()
-
-            if (username == "adal" && password == "123"){
-                val intent = Intent(this@MainActivity, Home::class.java)
-                startActivity(intent)
-            }else{
-                Toast.makeText(this@MainActivity, "Correo o contraseña incorrectos", Toast.LENGTH_SHORT).show()
-            }
-
-
-        }
-
-
-        menu.setOnClickListener {
-            val send = Intent(this, Home::class.java)
-            startActivity(send)
-        }
-
-
-
-    private fun isValid(username: String, password: String): Boolean {
-        return true
-    }*/
     }
 }
